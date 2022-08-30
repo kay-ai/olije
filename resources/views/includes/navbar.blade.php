fa94d6dab26f03a0c98253083f8fb5f91b4db7f3
Favour Obasi <53056853+kay-ai@users.noreply.github.com>
2022-08-30 01:13:45 +0100

    Welcome page hero section

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,51 @@
+<nav>
+    <nav class="navbar navbar-expand-md navbar-dark">
+        <img class="navbar-brand-logo" src="{{asset('img/logo.png')}}">
+        <a class="navbar-brand" href="#">Olije</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse nav-content" id="collapsibleNavId">
+            <form class="form-inline mr-auto">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input class=" o-input" type="text" placeholder="lenovo">
+            </form>
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Messages</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Saved (10)</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Premium Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">My Advert</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Wallet</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Job</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Forum</a>
+                </li>
+                {{-- <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="#">Action 1</a>
+                        <a class="dropdown-item" href="#">Action 2</a>
+                    </div>
+                </li> --}}
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <a id="" class="btn btn-light px-5" href="#" role="button">Sell</a>
+            </ul>
+        </div>
+    </nav>
+    <!-- Nav tabs -->
+</nav>
+
